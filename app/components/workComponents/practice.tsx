4bf4a690b566796dfd621bb214393aed507f13ca
Boboe16 <jerixmodz@gmail.com>
2025-01-22 01:15:19 +0800

    pepep

--- app/components/workComponents/practice.tsx
+++ app/components/workComponents/practice.tsx
@@ -39,7 +39,7 @@ function SvgComponent({ svgColor }: { svgColor: string }) {
       </text>
       <defs>
         <style>
-          @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
+          @import url(&apos;https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap&apos;s);
         </style>
       </defs>
     </svg>
